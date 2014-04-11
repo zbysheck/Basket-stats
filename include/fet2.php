644e78c4587b167f308b0e6c3a7cccf064fec292
Zbigniew Kisły <zbyshekh@gmail.com>
2014-04-11 15:46:40 +0200

    edytowanie działa WRESZCIE!

--- include/fet2.php
+++ include/fet2.php
@@ -1,2 +1,15 @@
-			Nazwa drużyny: <input type="text" name="team_name">
+			<form action="edit.php" method="post">
+			Nazwa drużyny: <input type="text" name="team_name" value="
+<?php
+	echo team_name($_POST['team']);
+?>
+">
+			<input type="hidden" name="team" value="
+<?php
+	echo $_POST['team'];
+?>
+">
+			<input type="hidden" name="sub" value="yes">
+			<input type="hidden" name="case" value="addteam">
 			<br/><input type="submit">
+			</form>
