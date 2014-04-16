2158391271dcd52388035ce8707c15b089a63a6c
Zbigniew Kisły <zbyshekh@gmail.com>
2014-04-16 11:27:23 +0200

    usuniete dwójki

--- include/fet2.php
+++
@@ -1,9 +0,0 @@
-<?php			
-	echo '<form action="" method="post">
-	Nazwa drużyny: <input type="text" name="team_name" value="';
-	echo team_name($_POST['team']);
-	echo '">
-			<input type="hidden" name="team" value="';
-	echo $_POST['team'] .'">';
-	buttons("chooseteam");
-	echo '</form>';
