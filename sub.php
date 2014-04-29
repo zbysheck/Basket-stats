bb412780d257847adf599b4013395c35949421db
Zbigniew Kisły <zbyshekh@gmail.com>
2014-04-29 09:33:46 +0200

    1.0

--- sub.php
+++ sub.php
@@ -1,6 +1,6 @@
 <?php
 	$con=mysqli_connect("localhost","root","","basket2");
-	include "functions.php";
+	require_once("functions.php");
 	// Check connection
 	if (mysqli_connect_errno()){
 		echo "Failed to connect to MySQL: " . mysqli_connect_error();
