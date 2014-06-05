5957692e96bb633ad62949a3eac37d878ab4d0c7
unknown <zkisly@zkisly.avena.polska>
2014-06-05 09:25:18 +0200

    logiczny interface

--- game.php
+++ game.php
@@ -1,9 +1,108 @@
 <?php
-require_once("functions.php");
-	$query='SELECT * FROM game';
+	require_once("functions.php");
+	$game = $_GET["edit"] ?: 1;
+
+	if(isset($_POST['addplayer'])){
+		//var_dump($_POST);
+			$sql="INSERT INTO player (name)
+			VALUES
+			('$_POST[addplayer]')";
+		if (!mysqli_query($con,$sql)){
+			die('Error: ' . mysqli_error($con));
+		}
+		echo "zawodniczka dodana";
+	}
+
+	if(isset($_POST['addstat'])){
+		//var_dump($_POST);
+			$sql="INSERT INTO stat (player_id, game_id)
+			VALUES
+			('$_POST[addstat]','$game')";
+		if (!mysqli_query($con,$sql)){
+			die('Error: ' . mysqli_error($con));
+		}
+		echo "zawodniczka dodana";
+	}
+
+	if(isset($_POST['stat_id'])){
+		//var_dump($_POST);
+		$labels=array("minutes", "fg3", "fga3", "fg2", "fga2", "fg1", "fga1", "orb", "drb", "assists", "fauls", "turnovers", "steals", "blocks");
+	
+		$sql = "";
+			foreach ($labels as $i){
+				$sql .= $i . " = " . $_POST[$i] . ", ";
+			}
+			$sql=rtrim($sql, " ,") . ' ';
+			update_table("stat", $_POST['stat_id'], $sql, "statystyki");
+	}
+
+	if(isset($_GET['delete'])){
+		//var_dump($_GET);
+		remove("stat", $_GET['delete'], "statystyki");
+	}
+
+	//var_dump($_POST);
+?>
+	<form action="" method="post">
+	Nowa zawodniczka: <input type="text" name="addplayer">
+	<input type="submit"></form>
+
+			<form action="" method="post">
+			<b>Dodaj mecze:</b><br> 
+			<select name="addstat">
+			<?php
+				$query='SELECT * FROM player';
+				$result = mysqli_query(connect(),$query);
+				while($row = mysqli_fetch_array($result)){
+					echo "<option value=";
+					echo $row['id'];
+					echo ">";
+					echo $row['name'];
+					echo "</option>";
+				}?>
+			</select>
+			<input type="submit" value="Dodaj zawodniczke do meczu">
+		</form>
+<?php
+	$query='SELECT *, stat.id AS stat_id FROM stat INNER JOIN player ON stat.player_id=player.id WHERE game_id=';
+	$query.=$game;
 	$result = mysqli_query(connect(),$query);
+	$labels=array("minutes", "fg3", "fga3", "fg2", "fga2", "fg1", "fga1", "orb", "drb", "assists", "fauls", "turnovers", "steals", "blocks");
+	echo "<table border='1'><tr><th>Zawodniczka</th>";
+	foreach ($labels as $i) {
+		echo "<th>$i</th>";
+	}
+	echo "<th>opcje</th></tr>";
+
 	while($row = mysqli_fetch_array($result)){
 		//var_dump($row);
-		echo "<a href=>".$row["team1_id"]." vs. ".$row["team2_id"]." (".$row["game_date"].")</a><br/>";
+		//echo "<a href=>".$row["team1_id"]." vs. ".$row["team2_id"]." (".$row["game_date"].")</a><br/>";
+		echo "<tr>";
+		echo '<form action="" method="post"><td>
+				<input type="text" readonly name="name" value="';
+		echo $row['name'];
+		echo "\">";
+		echo "<input type=\"hidden\" name=\"player_id\" value=\"";
+		echo $row['player_id'];
+		echo "\">";
+		echo "<input type=\"hidden\" name=\"stat_id\" value=\"";
+		echo $row['stat_id'];
+		echo "\">";
+
+		foreach ($labels as $i) {
+		echo "</td><td style=\"width: 20px\">";
+				echo "<input type=\"number\" name=".$i." style=\"width: 45px\" value=\"";
+				echo $row[$i];
+				echo '">';
+		echo "</td>";
+		}
+		echo "<td><input type='submit' value='aktualizuj'/>";
+		echo "<input type='hidden' name='case' value='update'>";
+		echo "<input type='hidden' name='id' value='";
+		echo $row['id'];
+		echo "'>";//admin.php?page=basket/basket.php&tab=game
+		echo "</form><a href=admin.php?page=basket/basket.php&tab=game&season=".$season."&delete=".$row["stat_id"].">[usuń]</a>
+		</td></tr>";
 	}
+	echo "</table>";
 	?>
--- seasons.php
+++ seasons.php
@@ -7,12 +7,21 @@ if(isset($_POST['case']) && $_POST['case']=="chooseseason"){
 		if (!mysqli_query($con,$sql)){
 			die('Error: ' . mysqli_error($con));
 		}
-		}
+}
+
+if(isset($_GET['delete'])){
+	//var_dump($_GET);
+	remove("season", $_GET['delete'], "sezonu");
+}
+
+if(isset($_GET['activate'])){
+	update_option('BS_active', $_GET['activate']);
+}
+$active=get_option('BS_active');
 		?>
-	<br/>
 		<form action="" method="post">
 			<b>Dodaj:</b><br> 
-			<select name="year" onchange="this.form.submit()">
+			<select name="year">
 			<?php
 				for($i=1990;$i<=2050;$i++){
 					echo "<option value=\"$i\"";
@@ -25,13 +34,20 @@ if(isset($_POST['case']) && $_POST['case']=="chooseseason"){
 				}?>
 			</select>
 			<input type="hidden" name="case" value="chooseseason">
+			<input type="submit" value="Dodaj Sezon">
 		</form>
 <?php
+//var_dump($_GET);
 
-
-	$query='SELECT * FROM season';
+	$query='SELECT * FROM season ORDER BY year';
 	$result = mysqli_query(connect(),$query);
 	while($row = mysqli_fetch_array($result)){
 		//var_dump($row);
-		echo "<a href=>".season($row["year"])."</a><br/>";
+		echo "<a href=admin.php?page=basket/basket.php&tab=season&season=".$row["id"].">".season($row["year"])." (edytuj)</a>
+		<a href=admin.php?page=basket/basket.php&tab=seasons&delete=".$row["id"].">[usuń]</a>  ";
+		if($active==$row["id"]){
+			echo '<font color="lime">[aktywny]</font><br/>';
+		}else{
+			echo "<a href=admin.php?page=basket/basket.php&tab=seasons&activate=".$row["id"].">[aktywuj]</a><br/>";
+		}
 	}
